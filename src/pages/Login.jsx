3874efd20943c3d70cfdf7b39c7ea0dec28a85b8
Developer@2446 <balu2446madhu@gmail.com>
2025-01-11 03:13:48 +0530

    captcha fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -380,7 +380,7 @@ const Login = () => {
                 <div className="flex justify-center mb-4 mt-8">
                 <Turnstile
                 
-            sitekey="0x4AAAAAAA3HP5RN6qhb67vx" // Replace with your site key
+            sitekey="0x4AAAAAAA1Y9hSf6wBjYC09" // Replace with your site key
             onVerify={(token) => {
               // console.log("Captcha token:", token);
               setCaptchaValue(token);
