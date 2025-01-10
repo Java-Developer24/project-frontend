355857999a7763389f01d37f18ced6a8072d26d5
Developer@2446 <balu2446madhu@gmail.com>
2025-01-10 21:54:42 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindcssAnimate from "tailwindcss-animate";
+export default  {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,jsx}",
@@ -83,5 +84,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
