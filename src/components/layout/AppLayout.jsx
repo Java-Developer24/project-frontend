ef07ac34539d5c950e379e5f6493882b618a0e91
Developer@2446 <balu2446madhu@gmail.com>
2025-01-15 10:55:01 +0530

    Ui updates

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,19 +1,19 @@
+/* eslint-disable react/display-name */
 import React from "react";
 import Header from "./Header";
-
 import { Toaster } from "react-hot-toast";
 
 const AppLayout = () => (WrappedComponent) => {
   return (props) => {
     return (
-      <div className="min-h-screen flex flex-col">
+      <>
+        {/* <Title /> */}
         <Toaster position="bottom-right" reverseOrder={false} />
         <Header />
-        <div className="md:container px-[1rem] flex-grow">
+        <div className="md:container px-[1rem]">
           <WrappedComponent {...props} />
         </div>
-       
-      </div>
+      </>
     );
   };
 };
