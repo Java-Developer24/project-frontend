f66b6fafe1e583a406bf7f02b50132ad84813dcd
Developer@2446 <balu2446madhu@gmail.com>
2025-02-07 05:56:53 +0530

    changes

--- src/pages/CheckOtp.jsx
+++ src/pages/CheckOtp.jsx
@@ -23,7 +23,7 @@ const CheckOtp = () => {
 
     try {
       const response = await axios.get(
-        `http://localhost:3000/api/server/check-otp?otp=${encodedotp}&api_key=${apiKey}`
+        `/api/server/check-otp?otp=${encodedotp}&api_key=${apiKey}`
       );
 
       setOtp("");
